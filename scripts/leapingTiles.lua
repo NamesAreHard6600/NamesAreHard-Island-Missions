c734653804cc89354fa86abc21a4330ba43404bd
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2023-06-17 21:15:48 -0700

    Building Transport and Name Changes

--- scripts/leapingTiles.lua
+++ scripts/leapingTiles.lua
@@ -6,26 +6,103 @@ local a = ANIMS
 --Global Copy
 NAH_Missions_leapingTiles = mod.libs.leapingTiles
 
-modApi:appendAsset("img/units/mission/propeller.png",mod.resourcePath.."img/units/mission/propeller.png")
-modApi:appendAsset("img/units/mission/propellera.png",mod.resourcePath.."img/units/mission/propellera.png")
+local suffixes = {
+  "",
+  "_1_1",
+  "_0_1",
+  "_2_2",
+  "_1_2",
+  "_0_2",
+}
 
+--Tileset
 modApi:appendAsset("img/combat/tiles_grass/invisible.png",mod.resourcePath.."img/tileset/invisible.png")
 modApi:appendAsset("img/combat/tiles_grass/moving_tile.png",mod.resourcePath.."img/tileset/moving_tile.png")
 
-a.NAH_Leaping_Tile = a.BaseUnit:new{Image = "units/mission/propeller.png", PosX = -37, PosY = -8}
-a.NAH_Leaping_Tilea = a.NAH_Leaping_Tile:new{Image = "units/mission/propellera.png"}
+--"pawns"
+for i, suffix in ipairs(suffixes) do
+  modApi:appendAsset("img/units/mission/leaping_tile"..suffix..".png",mod.resourcePath.."img/units/mission/leaping_tile"..suffix..".png")
+end
 
+
+--Fun Copy Paste Time
+
+--Pawn ANIMS
+a.NAH_Leaping_Tile = a.BaseUnit:new{Image = "units/mission/leaping_tile.png", PosX = -37, PosY = -13}
+a.NAH_Leaping_Tilea = a.NAH_Leaping_Tile:new{}
+
+a.NAH_Leaping_Tile_1_1 = a.NAH_Leaping_Tile:new{Image = "units/mission/leaping_tile_1_1.png"}
+a.NAH_Leaping_Tile_1_1a = a.NAH_Leaping_Tile_1_1:new{}
+
+a.NAH_Leaping_Tile_0_1 = a.NAH_Leaping_Tile:new{Image = "units/mission/leaping_tile_0_1.png"}
+a.NAH_Leaping_Tile_0_1a = a.NAH_Leaping_Tile_0_1:new{}
+
+a.NAH_Leaping_Tile_2_2 = a.NAH_Leaping_Tile:new{Image = "units/mission/leaping_tile_2_2.png"}
+a.NAH_Leaping_Tile_2_2a = a.NAH_Leaping_Tile_2_2:new{}
+
+a.NAH_Leaping_Tile_1_2 = a.NAH_Leaping_Tile:new{Image = "units/mission/leaping_tile_1_2.png"}
+a.NAH_Leaping_Tile_1_2a = a.NAH_Leaping_Tile_1_2:new{}
+
+a.NAH_Leaping_Tile_0_2 = a.NAH_Leaping_Tile:new{Image = "units/mission/leaping_tile_0_2.png"}
+a.NAH_Leaping_Tile_0_2a = a.NAH_Leaping_Tile_0_2:new{}
 
 NAH_Leaping_Tile = {
   Name = "Moving Tile",
-  Health = 2,
-  MoveSpeed = 4,
-  Image = "NAH_Leaping_Tile", --Change
-  SoundLocation = "/support/civilian_tank/", --Probably Change
+  Image = "NAH_Leaping_Tile",
   DefaultTeam = TEAM_NONE,
-  --SkillList = {"NAH_ExcavatorSkill"},
   IsPortrait = false,
   Massive = true,
   Flying = true,
 }
 AddPawn("NAH_Leaping_Tile")
+
+--_currenthealth_maxhealth
+NAH_Leaping_Tile_1_1 = {
+  Name = "Moving Tile",
+  Image = "NAH_Leaping_Tile_1_1", --This would change
+  DefaultTeam = TEAM_NONE,
+  IsPortrait = false,
+  Massive = true,
+  Flying = true,
+}
+AddPawn("NAH_Leaping_Tile_1_1")
+
+NAH_Leaping_Tile_0_1 = {
+  Name = "Moving Tile",
+  Image = "NAH_Leaping_Tile_0_1", --This would change
+  DefaultTeam = TEAM_NONE,
+  IsPortrait = false,
+  Massive = true,
+  Flying = true,
+}
+AddPawn("NAH_Leaping_Tile_0_1")
+
+NAH_Leaping_Tile_2_2 = {
+  Name = "Moving Tile",
+  Image = "NAH_Leaping_Tile_2_2", --This would change
+  DefaultTeam = TEAM_NONE,
+  IsPortrait = false,
+  Massive = true,
+  Flying = true,
+}
+AddPawn("NAH_Leaping_Tile_2_2")
+
+NAH_Leaping_Tile_1_2 = {
+  Name = "Moving Tile",
+  Image = "NAH_Leaping_Tile_1_2", --This would change
+  DefaultTeam = TEAM_NONE,
+  IsPortrait = false,
+  Massive = true,
+  Flying = true,
+}
+AddPawn("NAH_Leaping_Tile_1_2")
+
+NAH_Leaping_Tile_0_2 = {
+  Name = "Moving Tile",
+  Image = "NAH_Leaping_Tile_0_2", --This would change
+  DefaultTeam = TEAM_NONE,
+  IsPortrait = false,
+  Massive = true,
+  Flying = true,
+}
+AddPawn("NAH_Leaping_Tile_0_2")
--- scripts/libs/leapingTiles.lua
+++ scripts/libs/leapingTiles.lua
@@ -6,13 +6,13 @@
 
 local this = {}
 
-local function leap_tile(p1,p2,effect)
+local function leap_tile(p1,p2,effect,spawn_pawn)
   local dir = GetDirection(p1-p2)
 
-  effect:AddLeap(Board:GetPath(p1, p2, PATH_FLYER), NO_DELAY) --Leaps the tile (kinda)
+  effect:AddLeap(Board:GetPath(p1, p2, PATH_FLYER), NO_DELAY) --Leaps the tile (technically)
   --The second leap moves any pawns on it, if there's a pawn that's not a leap tile
   local pawn = Board:GetPawn(p1)
-  if pawn and pawn:GetType() ~= "NAH_Leaping_Tile" then
+  if pawn and not pawn:GetType():find("^"..spawn_pawn) then
     effect:AddLeap(Board:GetPath(p1, p2, PATH_FLYER), NO_DELAY)
   end
 
@@ -40,7 +40,7 @@ local function get_effect(from, to, spawn_pawn)
   for i, point in ipairs(from) do
     local p1 = point
     local p2 = to[i]
-    ret = leap_tile(p1,p2,ret)
+    ret = leap_tile(p1,p2,ret,spawn_pawn)
   end
 
   ret:AddDelay(0.45) --Timing based on in game testing
@@ -60,6 +60,10 @@ end
 --@param from: The list of points we are moving tiles from
 --@param to: The list of points we are moving tiles to, as matched with from (index 1 of from goes to index 1 of to)
 --@param spawn_pawn: The type of the pawn that is used to make the tile leap
+--For leaping buildings, you should have additional pawns for each of the different building types
+--Name it the same as spawn_pawn and append _(CurrentHealth)_(MaxHealth)
+--Ex: A pawn that has a 2 building that's damaged once should be named spawn_pawn_1_2
+--You realistically should have _1_1, _0_1, _2_2, _1_2, _0_2,
 --@param custom_tile: The custom tile that will be set to the new location - empty if it's a normal tile
 --@param invisible_tile: The custom tile for your tileset that is invisible - it should just be an empty sprite
 --Error Catching:
@@ -88,7 +92,7 @@ function this:move_tiles(from, to, spawn_pawn, custom_tile, invisible_tile)
       if false or
       not Board:IsValid(point) or --Not Valid
       not Board:IsValid(point2) or --Not Valid
-      Board:GetTerrain(point) ~= TERRAIN_ROAD or --From isn't a ground tile, we can't throw it.
+      not (Board:GetTerrain(point) == TERRAIN_ROAD or Board:GetTerrain(point) == TERRAIN_BUILDING) or --From isn't a ground tile or building, we can't throw it.
       (Board:GetTerrain(point2) ~= TERRAIN_HOLE and not list_contains(from, point2)) or --To isn't a hole, it can't land safely, unless it's also moving (why we repeat, in case a from isn't valid)
       point == point2 then --We're trying to jump to the same point, that breaks things, so just don't
         table.insert(bad_indexes,i)
@@ -127,34 +131,42 @@ function this:move_tiles(from, to, spawn_pawn, custom_tile, invisible_tile)
     return not Board:IsBusy() --Wait for webs to be gone
   end,
   function()
-    --Add and store tile pawns, and set tiles to invisible tiles
+    --Add and store tile pawns, and set tiles to invisible tiles, and store buildings
     local leaping_tiles = {}
     for i, point in ipairs(from) do
       local pawn = PAWN_FACTORY:CreatePawn(spawn_pawn)
+      local current_health = 0
+      local max_health = 0
+      if Board:GetTerrain(point) == TERRAIN_BUILDING then
+        current_health = Board:GetHealth(point)
+        max_health = Board:GetMaxHealth(point)
+        pawn = PAWN_FACTORY:CreatePawn(spawn_pawn.."_"..current_health.."_"..max_health)
+        Board:SetTerrain(point,TERRAIN_ROAD) --This would have to move, but for testing, it's here
+      end
       Board:AddPawn(pawn,point)
       --pawn:MoveToBottom() --Not yet
       Board:SetCustomTile(point,invisible_tile)
-      table.insert(leaping_tiles,pawn:GetId())
+      table.insert(leaping_tiles,{pawn:GetId(),current_health,max_health})
     end
 
     --Get and trigger the leaps and such
-    effect = get_effect(from,to)
+    effect = get_effect(from,to,spawn_pawn)
     Board:AddEffect(effect)
 
-    for k, id in ipairs(leaping_tiles) do
-      local pawn = Board:GetPawn(id)
+    for k, list in ipairs(leaping_tiles) do
+      local pawn = Board:GetPawn(list[1]) --id
       pawn:MoveToBottom() --Move it to the bottom so it's visually on the bottom
     end
 
     --Wait most of the time, then...
     modApi:scheduleHook(700, function()
       --Remove the invisible tiles
-      for i, point in ipairs(from) do
+      for i, point in ipairs(from) do --This could happen a lot sooner
         Board:SetTerrain(point,TERRAIN_HOLE)
         Board:SetCustomTile(point,"") --Reset Invisible
       end
       --Add invisible tiles to end point
-      for i, point in ipairs(to) do
+      for i, point in ipairs(to) do --But this part still needs to start late
         Board:SetTerrain(point,TERRAIN_ROAD)
         Board:SetCustomTile(point,invisible_tile)
       end
@@ -162,11 +174,18 @@ function this:move_tiles(from, to, spawn_pawn, custom_tile, invisible_tile)
 
     --Wait for leap to finish: It takes 800 msec
     modApi:scheduleHook(800, function()
-      --Kill All Tile Pawns and Place Tiles Back
-      for k, id in ipairs(leaping_tiles) do
-        local pawn = Board:GetPawn(id)
-        Board:SetCustomTile(pawn:GetSpace(),custom_tile)
+      --Kill All Tile Pawns and Place Tiles Back and add in buildings
+      for k, list in ipairs(leaping_tiles) do
+        local pawn = Board:GetPawn(list[1]) --id
+        local point = pawn:GetSpace()
+        Board:SetCustomTile(point,custom_tile)
         Board:RemovePawn(pawn)
+        --Add in buildings
+        if list[3] ~= 0 then
+          Board:SetTerrain(point,TERRAIN_BUILDING)
+          Board:SetHealth(point,list[2],list[3])
+          Board:SetPopulated(true,point)
+        end
       end
     end)
   end)
@@ -176,9 +195,14 @@ end
 --If you have multiple tiles to move, it's highly suggested you use move_tiles() to catch errors
 --@params see move_tile
 function this:move_tile(from, to, spawn_pawn, custom_tile, invisible_tile)
-  assert(type(from) == 'userdata', "from is not a point")
-  assert(type(to) == 'userdata', "to is not a point")
+  assert(type(from) == 'userdata', "LEAPING TILES: from is not a point")
+  assert(type(to) == 'userdata', "LEAPING TILES: to is not a point")
   this:move_tiles({from},{to},spawn_pawn,custom_tile,invisible_tile)
 end
 
 return this
+
+--[[DEBUG
+Board:SetHealth(Point(5,5),2,2); Board:SetHealth(Point(4,5),1,2); Board:SetHealth(Point(6,5),0,2); Board:SetHealth(Point(5,4),1,1); Board:SetHealth(Point(5,6),0,1);
+
+]]
